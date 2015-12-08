34287266ec938801c45c2bba761c79b0a7629de8
Damien Bargiacchi <icymidnight@gmail.com>
2015-12-07 21:39:45 -0800

    s/count/floor/

--- day01/p1.py
+++ day01/p1.py
@@ -2,18 +2,18 @@
 
 import sys
 
-def count_floor(line):
-    count = 0
+def find_floor(line):
+    floor = 0
     for c in line:
         if c == '(':
-            count += 1
+            floor += 1
         elif c == ')':
-            count -= 1
+            floor -= 1
         else:
             raise RuntimeError("Invalid input: \"%s\" - '%s'" % (line, c))
-    return count
+    return floor
 
 with open(sys.argv[1], "r") as f:
     for line in f:
         line = line.rstrip()
-        print "%s = floor %d" % (line, count_floor(line))
+        print "%s = floor %d" % (line, find_floor(line))
