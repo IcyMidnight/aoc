460d274173fc1575ea2e0382aa74053ec44f2489
Damien Bargiacchi <icymidnight@gmail.com>
2020-12-03 00:00:13 -0800

    Uniform layout

--- 2020/newday.sh
+++ 2020/newday.sh
@@ -4,14 +4,14 @@ days=`ls -d day* | wc -l`
 
 (( new_day = $days + 1))
 
-echo $new_day
-
 day="day${(l:2::0:)new_day}"
 
 mkdir $day
 cp ./template.py "${day}/${day}.py"
-touch "${day}/test.txt"
+touch "${day}/example.txt"
 touch "${day}/input.txt"
 
 cd $day
-ls -ahl
+
+pwd
+ls -Ahl
