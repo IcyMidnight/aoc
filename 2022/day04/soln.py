a38e49510ffef1aafe76339efe14aa263cdacc1d
Damien Bargiacchi <icymidnight@gmail.com>
2022-12-04 16:36:15 -0800

    2022 Day 04 Cleaned

--- 2022/day04/soln.py
+++ 2022/day04/soln.py
@@ -4,42 +4,6 @@ import sys
 
 from pprint import pp
 
-from collections import defaultdict, Counter, deque
-
-import re
-import math
-
-def extract(s):
-    return [int(x) for x in re.findall(r'(-?\d+).?', s)]
-
-def vadd(v1, v2):
-    return tuple(x + y for x, y in zip(v1, v2))
-
-def ichr(i):
-    return chr(ord('a') + i)
-
-def iord(c):
-    return ord(c.lower()) - ord('a')
-
-def optidx(d, opt=max, nth=0):
-    if not isinstance(d, dict):
-        d = dict(enumerate(d))
-    rv = opt(d.values())
-    return [i for i, v in d.items() if v == rv][nth], rv
-
-LETTERS = "abcdefghijklmnopqrstuvwxyz"
-
-UP, RIGHT, DOWN, LEFT = VDIRS = (0, -1), (1, 0), (0, 1), (-1, 0),
-DIRS = {'N': UP, 'E': RIGHT, 'S': DOWN, 'W': LEFT }
-ALL_DIRS = [(x, y) for x in [-1,0,1] for y in [-1,0,1] if not x == y == 0]
-
-def turn(v, d='left'):
-    n = -1 if d == 'left' else 1
-    return VDIRS[(VDIRS.index(v) + n + len(VDIRS))%len(VDIRS)]
-
-
-##############################
-
 def set_completely_contains(s1, s2):
     intersection = s1 & s2
     return intersection == s1 or intersection == s2
